92914d7a6eec68011d9ce67e92d121f9bc7a730c
Kurt Vermillion <kurtv0727@gmail.com>
2022-06-29 13:20:23 -0400

    commit tree 1e620b189c0bc4051f85b8d4c93bdc38053b031e

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location){
+    if (location >= 42)
+    return (location - 42)
+    if (location < 42) 
+    return (42 - location)
+}
+
+function distanceFromHqInFeet(location){
+    return distanceFromHqInBlocks(location) * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    if (start > destination) 
+    return (start - destination) * 264
+    if (destination > start)
+    return (destination - start) *264
+}
+
+function calculatesFarePrice(start, destination){
+    if ((distanceTravelledInFeet(start, destination)) <= 400)
+    return 0
+    if ((distanceTravelledInFeet)(start, destination) < 2000)
+    return ((((distanceTravelledInFeet)(start, destination)) -400) * (.02)) 
+    if ((distanceTravelledInFeet)(start, destination) < 2500)
+    return 25
+    if ((distanceTravelledInFeet)(start, destination) > 2500)
+    return "cannot travel that far"
+}
